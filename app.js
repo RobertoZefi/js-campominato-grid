df5f9d5ce02ce58941f3d810a43095f81452d0ea
RobertoZefi <zefi.roberto2001@gmail.com>
2023-02-09 19:03:46 +0100

    FEAT style added

--- app.js
+++ app.js
@@ -4,16 +4,37 @@ let latoCelle = 10
 let celleTotali = latoCelle * latoCelle
 console.log(celleTotali)
 
+
 const grigliaElement = document.querySelector('.griglia')
 
-for (let i = 0; i < celleTotali; i++){
-    let cella = i +1 
+const button = document.querySelector('.button')
+
+button.addEventListener ('click', function(){
+    grigliaElement.classList.add('active')
+})
+
+
+
+for (let i = 1; i <= celleTotali; i++){
+    let cella = i  
     console.log(cella)
 
     let divString = `<div class="celle" style="width:calc(100% / ${latoCelle})">${cella}</div>`
-    console.log(divString)
+    // console.log(divString)
 
     grigliaElement.innerHTML += divString
+
 }
 
 const celleElement = document.querySelectorAll('.celle')
+console.log (celleElement)
+
+for (let i = 0; i < celleElement.length; i++){
+    let cella = celleElement[i]
+    cella.addEventListener ('click', function(){
+        console.log(i + 1)
+        cella.style.backgroundColor = 'darkred';
+        cella.style.color = 'white';
+    })
+}
+
